a2a7449faa8a5babd89db024e734c028c8a9eba7
devaka_kum <k.devaka@directfn.com>
2022-07-03 23:31:24 +0530

    Title Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "./components/Home/Home";
-import { Box, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    <Box className="App">
-      <Typography component={"h1"}>Employee Manager </Typography>
+    <Container className="App">
+      <Typography variant="h4" component="div" gutterBottom>Employee Manager </Typography>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Box>
+    </Container>
   );
 }
 
--- src/components/Home/Summary.js
+++ src/components/Home/Summary.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Box } from "@mui/material";
 import { useEmployees } from "../../customHooks/useEmployees";
 import { TableComponent } from "../common/Table";
 import { SummaryColumns } from "./const";
@@ -7,8 +8,8 @@ export const Summary = () => {
   const { employeeData } = useEmployees();
   
   return (
-    <div>
+    <Box>
       <TableComponent columns={SummaryColumns} data={employeeData} />
-    </div>
+    </Box>
   );
 };
