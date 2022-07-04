3bb6c056e66366d73a8a69ca1c2573a6bdb1ef20
devaka_kum <k.devaka@directfn.com>
2022-07-04 23:26:05 +0530

    Redirection to add

--- src/components/Home/Summary.js
+++ src/components/Home/Summary.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import { useEmployees } from "../../customHooks/useEmployees";
 import { TableComponent } from "../common/Table";
@@ -6,12 +7,13 @@ import { SummaryColumns } from "./const";
 import { ButtonComponent } from "../common/Button";
 
 export const Summary = () => {
+  let navigate = useNavigate();
   const { employeeData } = useEmployees();
   
   return (
     <Box>
         <Box sx={{textAlign: 'right', marginBottom: '1em'}}>
-        <ButtonComponent label="Add"/>
+        <ButtonComponent label="Add" onClick={()=>  navigate("../add")}/>
         </Box>
       <TableComponent columns={SummaryColumns} data={employeeData} />
     </Box>
